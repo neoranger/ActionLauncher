e4b29a4701e82c7c343c0bc50d7fd7a78ecef01a
NeoRanger <elgranpote@gmail.com>
2016-02-01 17:51:41 -0300

    Fixes

--- action_launcher.py
+++ action_launcher.py
@@ -17,13 +17,13 @@ TOKEN =  token.token_id
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
 #############################################
 #Listener
-def listener(messages): 
-    for m in messages: 
-        cid = m.chat.id 
+def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
+    for m in messages: # Por cada dato 'm' en el dato 'messages'
+        cid = m.chat.id # Almacenaremos el ID de la conversación.
         if m.content_type == 'text':
-            print "[" + str(cid) + "]: " + m.text 
+            print "[" + str(cid) + "]: " + m.text # Y haremos que imprima algo parecido a esto -> [52033876]: /start
  
-bot.set_update_listener(listener) 
+bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba) 
 #############################################
 #Funciones
 @bot.message_handler(commands=['temp']) 
