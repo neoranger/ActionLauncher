7bfeb9f3170a6d58ca953753f83aa877be54d0f2
NeoRanger <elgranpote@gmail.com>
2016-02-29 17:19:49 -0300

    modified:   runbot.sh

--- runbot.sh
+++ runbot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+########################################################################
+You can edit the path to select the correct .py file for run the script
+########################################################################
+
 DIR=/home/$USER/Documents/git/ActionLauncher/
 cd $DIR
 python2 action_launcher.py
