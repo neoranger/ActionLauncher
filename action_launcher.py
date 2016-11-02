5402923246be936c1e42e06ba75b5057fbb91a34
Natanael Andrés Garrido <neoranger@openmailbox.org>
2016-11-02 14:29:38 +0000

    modified:   action_launcher.py

--- action_launcher.py
+++ action_launcher.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Action Launcher Bot: This is a bot how can shoots differents action depends commands
-# Code wrote by Zagur of PortalLinux.es and modified by NeoRanger of neositelinux.com.ar
+# Code wrote by Zagur of PortalLinux.es and modified by NeoRanger of neositelinux.com
 # For a good use of the bot please read the README file
 
 import telebot 
@@ -22,16 +22,87 @@ import StringIO
 TOKEN =  token.token_id
 bot = telebot.TeleBot(TOKEN) # Creating our bot object.
 bot.skip_pending=True # Skip the pending messages
-#############################################
-#Listener
+##################################################################
+#LISTENER                                                        #
+##################################################################
 def listener(messages):
 	for m in messages:
 		cid = m.chat.id
 	if m.content_type == 'text':
 		print ("[" + str(cid) + "]: " + m.text)
 bot.set_update_listener(listener) #  
-#############################################
-#Funciones
+##################################################################
+#FUNCIONES PRINCIPALES DEL BOT (CON SEGURIDAD)                   #
+##################################################################
+
+@bot.message_handler(commands=['temp']) 
+def command_temp(m): 
+    temp = commands.getoutput('sudo vcgencmd measure_temp')
+    send_message_checking_permission(m, '```' + temp + '```')
+    
+@bot.message_handler(commands=['df']) 
+def command_espacio(m): 
+    info = commands.getoutput('df -h')
+    send_message_checking_permission(m, '```' + info + '```')
+
+@bot.message_handler(commands=['uptime']) 
+def command_tiempo(m):
+    tiempo = commands.getoutput('uptime')
+    send_message_checking_permission(m, '```' + tiempo + '```')
+	
+@bot.message_handler(commands=['free']) 
+def command_libre(m): 
+    libre = commands.getoutput('free -m')
+    send_message_checking_permission(m, '```' + libre + '```')
+
+@bot.message_handler(commands=['info']) 
+def command_info(m): 
+    screenfetch = commands.getoutput('screenfetch -n')
+    send_message_checking_permission(m, '```' + screenfetch + '```')
+
+@bot.message_handler(commands=['who']) 
+def command_who(m): 
+    who = commands.getoutput('who')
+    send_message_checking_permission(m, '```' + who + '```')
+    
+@bot.message_handler(commands=['shutdown']) 
+def command_shutdown(m):
+	shutdown = commands.getoutput('poweroff')
+	send_message_checking_permission(m, '```' + shutdown + '```')
+    
+@bot.message_handler(commands=['reboot']) 
+def command_reboot(m):
+	reboot = commands.getoutput('reboot')
+	send_message_checking_permission(m, '```' + reboot + '```')
+	
+@bot.message_handler(commands=['repoup']) 
+def command_repoup(m):
+	repoup = commands.getoutput('sudo apt-get update')
+	send_message_checking_permission(m, '```' + repoup + '```')
+	
+@bot.message_handler(commands=['sysup']) 
+def command_sysup(m):
+	sysup = commands.getoutput('sudo apt-get upgrade')
+	send_message_checking_permission(m, '```' + sysup + '```')
+	
+@bot.message_handler(commands=['distup']) 
+def command_distup(m):
+	distup = commands.getoutput('sudo apt-get dist-upgrade')
+	send_message_checking_permission(m, '```' + distup + '```')
+
+@bot.message_handler(commands=['osversion']) 
+def command_osversion(m):
+    osversion = commands.getoutput('lsb_release -a')
+    send_message_checking_permission(m, '```' + osversion + '```')
+#Otra forma: osversion = commands.getoutput('cat /etc/os-release')
+	
+##################################################################
+#FUNCIONES SIN SEGURIDAD (SIMPLES)                               #
+##################################################################
+@bot.message_handler(commands=['id']) 
+def command_id(m): 
+    cid = m.chat.id 
+    bot.send_message(cid, cid)		
 
 @bot.message_handler(commands=['ping']) 
 def command_test(m):
@@ -43,77 +114,14 @@ def command_ayuda(m):
     cid = m.chat.id
     bot.send_message(cid, "Comandos Disponibles: /help /ping /temp(admin) /free(admin) /df(admin) /uptime(admin) /info(admin) /who /repoup(admin) /sysup(admin) /distup(admin) /osversion(admin) /shutdown(admin) /reboot(admin) /start_nginx(admin) /stop_nginx(admin) /restart_nginx(admin) /bot_update(admin)")
 
-@bot.message_handler(commands=['temp']) 
-def command_temp(m): 
-    temp = commands.getoutput('sudo vcgencmd measure_temp')
-    send_message_checking_permission(m, temp)
-    
-@bot.message_handler(commands=['df']) 
-def command_espacio(m): 
-    info = commands.getoutput('df -h')
-    send_message_checking_permission(m, info)
-
-@bot.message_handler(commands=['uptime']) 
-def command_tiempo(m):
-    tiempo = commands.getoutput('uptime')
-    send_message_checking_permission(m, tiempo)
-	
-@bot.message_handler(commands=['free']) 
-def command_libre(m): 
-    libre = commands.getoutput('free -m')
-    send_message_checking_permission(m, libre)
-
-@bot.message_handler(commands=['info']) 
-def command_info(m): 
-    screenfetch = commands.getoutput('screenfetch -n')
-    send_message_checking_permission(m, screenfetch)
-
-@bot.message_handler(commands=['who']) 
-def command_who(m): 
-    who = commands.getoutput('who')
-    send_message_checking_permission(m, who)
-    
-@bot.message_handler(commands=['shutdown']) 
-def command_shutdown(m):
-	shutdown = commands.getoutput('poweroff')
-	send_message_checking_permission(m, shutdown)
-    
-@bot.message_handler(commands=['reboot']) 
-def command_reboot(m):
-	reboot = commands.getoutput('reboot')
-	send_message_checking_permission(m, reboot)
-	
-@bot.message_handler(commands=['repoup']) 
-def command_repoup(m):
-	repoup = commands.getoutput('sudo apt-get update')
-	send_message_checking_permission(m, repoup)
-	
-@bot.message_handler(commands=['sysup']) 
-def command_sysup(m):
-	sysup = commands.getoutput('sudo apt-get upgrade')
-	send_message_checking_permission(m, sysup)
-	
-@bot.message_handler(commands=['distup']) 
-def command_distup(m):
-	distup = commands.getoutput('sudo apt-get dist-upgrade')
-	send_message_checking_permission(m, distup)
-
-@bot.message_handler(commands=['osversion']) 
-def command_osversion(m):
-    osversion = commands.getoutput('lsb_release -a')
-    send_message_checking_permission(m, osversion)
-#Otra forma: osversion = commands.getoutput('cat /etc/os-release')
-	
-@bot.message_handler(commands=['id']) 
-def command_id(m): 
-    cid = m.chat.id 
-    bot.send_message(cid, cid)		
-
 #@bot.message_handler(commands=['apache']) 
 #def command_test(m):
 #    cid = m.chat.id
 #    bot.send_document(cid, '/home/ubuntu/apache2.conf','rb')
 
+##################################################################
+#MANEJO DEL SERVIDOR NGINX Y UPDATE DEL BOT VIA GIT              #
+##################################################################
 @bot.message_handler(commands=['start_nginx']) 
 def command_start_nginx(m):
 	nginx_start = commands.getoutput('sudo service nginx start')
@@ -145,7 +153,9 @@ def send_message_checking_permission(m, response):
         return
     bot.send_message(cid, response)
     
-#############################################
-#Peticiones
+##################################################################
+#PETICIONES                                                      #
+##################################################################
 print ("Bot Started")
+print ("Thanks for using NeoBot. Please visit us: https://www.neositelinux.com")
 bot.polling(none_stop=True) # Con esto, le decimos al bot que siga funcionando incluso si encuentra algun fallo.
